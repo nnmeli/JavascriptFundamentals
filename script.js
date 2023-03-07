fd737a1a19a9b35091fc8e48aec1fc98de4698ef
nnmeli <melihtumur1907@gmail.com>
2023-03-07 21:31:54 +0300

    firt commit for js101 works

---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+
+// For first Commit 
+
+
+
+/*
+function calcAverage(skor1, skor2, skor3) {
+    return (skor1 + skor2 + skor3) / 3
+};
+
+function checkWinner(team1, team2) {
+    if (team1 > team2) {
+        return `Takım 1 kazandı. (${team1} vs ${team2})`;
+    } else if (team2 > team1) {
+        return `Takım 2 kazandı. (${team1} vs ${team2})`;
+    } else {
+        return null;
+    }
+}
+
+
+let Dolphins = calcAverage(44, 23, 71);
+let Koalas = calcAverage(65, 54, 49);
+
+console.log(checkWinner(Dolphins, Koalas))
+
+
+*/
